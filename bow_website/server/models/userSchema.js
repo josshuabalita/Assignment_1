438df1c00106021f787701695c60cf1315811134
josshuabalita <joshbalita@icloud.com>
2023-11-29 20:28:47 -0700

    Added Login Backend and installed 'bcrypt' package

---
+++ bow_website/server/models/userSchema.js
@@ -0,0 +1,24 @@
+const Mongoose = require('mongoose');
+const UserSchema = new Mongoose.Schema({
+
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+
+    password: {
+        type: String,
+        minLength: 6,
+        required: true,
+    },
+
+    role :{
+        type: String,
+        defualt: "Student",
+        required: true,
+    }
+});
+
+const User = Mongoose.model("user", UserSchema);
+module.exports = User;
--- bow_website/server/server.js
+++ bow_website/server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const cors = require('cors');
+const bcrypt = require('bcrypt')
 
 
 //import mongodb connection file
@@ -22,7 +23,119 @@ app.get('/', (req,res) => {
 app.listen(PORT, () => {
     console.log('Server connected on PORT 8080')
 })
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Login Pages 
+const User = require('./models/userSchema.js')
+
+//Register User
+app.post('/register', async(req, res, next) => {
+  const { username, password } = req.body
+  if (password.length < 6) {
+    return res.status(400).json({ message: "Password less than 6 characters" })
+  }
+  try {
+    await User.create({
+      username,
+      password,
+    }).then(user =>
+      res.status(200).json({
+        message: "User successfully created",
+        user,
+      })
+    )
+  } catch (err) {
+    res.status(401).json({
+      message: "User not successful created",
+      error: error.mesage,
+    })
+  }
+})
+
+
+//Check Login inputs
+app.post('/login', async(req,res,next) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username or Password not present",
+    })
+  }
+  try {
+    const user = await User.findOne({ username, password })
+    if (!user) {
+      res.status(401).json({
+        message: "Login not successful",
+        error: "User not found",
+      })
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user,
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "An error occurred",
+      error: error.message,
+    })
+  }
+})
+
+//Update role of a user
+app.put('/update', async(req, res, next) => {
+  const { role, id } = req.body
+  if (role && id) {
+    if (role === "instructor") {
+      await User.findById(id)
+      .then((user) => {
+        if (user.role !== "instructor") {
+          user.role = role;
+          user.save((err) => {
+            if (err) {
+              res
+                .status("400")
+                .json({ message: "An error occurred", error: err.message });
+              process.exit(1);
+            }
+            res.status("201").json({ message: "Update successful", user });
+          });
+        } else {
+          res.status(400).json({ message: "User is already an instructor" });
+        }
+      })
+      .catch((error) => {
+        res
+          .status(400)
+          .json({ message: "An error occurred", error: error.message });
+      });
+    } else {
+      res.status(400).json({
+        message: "Role is not instructor",
+      })
+    }
+  } else {
+    res.status(400).json({ message: "Role or Id not present" })
+  }
+})
+
+
+//Delete Users
+app.delete('/deleteUser', async(req, res, next) => {
+  const { id } = req.body
+  await User.findById(id)
+    .then(user => user.remove())
+    .then(user =>
+      res.status(201).json({ message: "User successfully deleted", user })
+    )
+    .catch(error =>
+      res
+        .status(400)
+        .json({ message: "An error occurred", error: error.message })
+    )
+})
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Populate Database with Courses Data JSON and just GET afterwards
 const coursesSchema = require('./models/coursesSchema.js');
@@ -244,20 +357,9 @@ app.get('/contact/student-form', async (req, res) => {
 });
 
 
-// fetching all students
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const mongoose = require('mongoose')
-const Student = require('./node_modules/@mongodb-js');
-const RegisteredCourses = require('./models/addedCourseSchema.js');
-
-
-mongoose.connect('mongodb://localhost:27017/portal', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-app.use(express.json());
-
+// fetching all students and registered courses
 app.get('/courses/student/registered/:studentId', async (req, res) => {
   try {
     const studentId = req.params.studentId;
